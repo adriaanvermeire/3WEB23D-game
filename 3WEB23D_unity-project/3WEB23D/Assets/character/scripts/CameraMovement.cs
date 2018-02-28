ae8703904c82625d1724a841e0a25c105dc62ec6
JeroenGoyvaerts <jeroen.goyvaerts@student.kdg.be>
2018-03-01 00:27:14 +0100

    Game character control system geimplementeerd
    daarbij nog een probllem gefixt waarbij de unity scen in WebGL niet renderde

---
+++ 3WEB23D_unity-project/3WEB23D/Assets/character/scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+
+    public float rotationSpeed = 3.5f;
+    private float Y;
+
+    // Use this for initialization
+    void Start () {
+        transform.rotation = Quaternion.Euler(0, 90f, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            transform.Rotate(new Vector3(0, -Input.GetAxis("Mouse X") * rotationSpeed, 0));
+            Y = transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(0, Y, 0);
+        }
+	}
+}
---
+++ 3WEB23D_unity-project/3WEB23D/Assets/character/scripts/CharacterMovement.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+public class CharacterMovement : MonoBehaviour {
+
+    public bool forwardsEnabled = false;
+    public bool backwardsEnabled = false;
+    public bool rightEnabled = false;
+    public bool leftEnabled = false;
+
+    public GameObject player;
+    private NavMeshAgent playerAgent;
+
+    public Button forwardsButton;
+    public Button backwardsButton;
+    public Button rightButton;
+    public Button leftButton;
+
+    public Transform forwardsWaypoint = null;
+    public Transform backwardsWaypoint = null;
+    public Transform rightWaypoint = null;
+    public Transform leftWaypoint = null;
+
+    private bool isInTrigger = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerAgent = player.GetComponent<NavMeshAgent>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(isInTrigger);
+
+        //check if player is in the trigger and wich button should be enabled or not
+        //also check if the button is pressed if so call GoToWaypointOnClick() with the corresponding waypoint
+        if (forwardsEnabled && isInTrigger)
+        {
+            forwardsButton.gameObject.SetActive(true);
+            forwardsButton.onClick.AddListener(() => GoToWaypointOnClick(forwardsWaypoint));
+            //Debug.Log("button enabled");
+        }
+        if (backwardsEnabled && isInTrigger)
+        {
+            backwardsButton.gameObject.SetActive(true);
+            backwardsButton.onClick.AddListener(() => GoToWaypointOnClick(backwardsWaypoint));
+            //Debug.Log("button enabled");
+        }
+        if (rightEnabled && isInTrigger)
+        {
+            rightButton.gameObject.SetActive(true);
+            rightButton.onClick.AddListener(() => GoToWaypointOnClick(rightWaypoint));
+            //Debug.Log("button enabled");
+        }
+        if (leftEnabled && isInTrigger)
+        {
+            leftButton.gameObject.SetActive(true);
+            leftButton.onClick.AddListener(() => GoToWaypointOnClick(leftWaypoint));
+            //Debug.Log("button enabled");
+        }
+        //end of previous check
+
+    }
+
+    private void GoToWaypointOnClick(Transform target)
+    {
+        playerAgent.SetDestination(target.position);
+    }
+
+    //check if player's collider has entered the trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("object is IN trigger");
+            //if the above is true set isInTrigger to true
+            isInTrigger = true;
+        }
+    }
+
+    //chek if player's collider has left the trigger
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("object LEFT trigger");
+            //if the above is true set isInTrigger to false
+            isInTrigger = false;
+            forwardsButton.gameObject.SetActive(false);
+            backwardsButton.gameObject.SetActive(false);
+            rightButton.gameObject.SetActive(false);
+            leftButton.gameObject.SetActive(false);
+        }
+    }
+}
