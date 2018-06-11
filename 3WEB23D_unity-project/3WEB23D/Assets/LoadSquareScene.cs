cc0e825ca946b0f1c4b46b892d4f37aeae18ab8b
JeroenGoyvaerts <jeroen.goyvaerts@student.kdg.be>
2018-06-11 17:58:58 +0200

    test

---
+++ 3WEB23D_unity-project/3WEB23D/Assets/LoadSquareScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSquareScene : MonoBehaviour {
+
+	public void loadSquareScene()
+    {
+        SceneManager.LoadScene("square-scene");
+    }
+}
---
+++ 3WEB23D_unity-project/3WEB23D/Assets/play.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class play : MonoBehaviour {
+
+    int amountAccessed = 0;
+	// Use this for initialization
+	void Start () {
+        if (amountAccessed == 0)
+        {
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.Play();
+            amountAccessed++;
+        }
+    }
+
+}
