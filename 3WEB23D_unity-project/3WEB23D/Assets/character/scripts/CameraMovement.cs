5c9b0ad9945458339e9326ced219ef859c1f5004
jeroen goyvaerts <jeroen.goyvaerts1@hotmail.com>
2018-06-11 23:27:55 +0200

    final build

--- 3WEB23D_unity-project/3WEB23D/Assets/character/scripts/CameraMovement.cs
+++ 3WEB23D_unity-project/3WEB23D/Assets/character/scripts/CameraMovement.cs
@@ -6,29 +6,38 @@ public class CameraMovement : MonoBehaviour {
 
 
     public float rotationSpeed = 3.5f;
+    public float rotationSpeedAndriod = 0.1f;
     public float startRotation = 0;
     private float Y;
-    private float pointer_x = Input.GetAxis("Mouse X");
+    private float pointer_x;
 
     // Use this for initialization
     void Start () {
         transform.rotation = Quaternion.Euler(0, startRotation, 0);
+        pointer_x = Input.GetAxis("Mouse X");
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButton(0))
+        if (SystemInfo.deviceType == DeviceType.Desktop)
         {
-            transform.Rotate(new Vector3(0, -Input.GetAxis("Mouse X") * rotationSpeed, 0));
-            Y = transform.rotation.eulerAngles.y;
-            transform.rotation = Quaternion.Euler(0, Y, 0);
+            if (Input.GetMouseButton(0))
+            {
+                pointer_x = Input.mousePosition[0];
+                transform.Rotate(new Vector3(0, -Input.GetAxis("Mouse X") * rotationSpeed, 0));
+                Y = transform.rotation.eulerAngles.y;
+                transform.rotation = Quaternion.Euler(0, Y, 0);
+            }
         }
-        if(Input.touchCount > 0)
+        if (SystemInfo.deviceType == DeviceType.Handheld)
         {
-            pointer_x = Input.touches[0].deltaPosition.x;
-            transform.Rotate(new Vector3(0, -pointer_x * rotationSpeed, 0));
-            Y = transform.rotation.eulerAngles.y;
-            transform.rotation = Quaternion.Euler(0, Y, 0);
+            if (Input.touchCount == 2)
+            {
+                pointer_x = Input.touches[0].deltaPosition.x;
+                transform.Rotate(new Vector3(0, -pointer_x * rotationSpeedAndriod, 0));
+                Y = transform.rotation.eulerAngles.y;
+                transform.rotation = Quaternion.Euler(0, Y, 0);
+            }
         }
 	}
 }
