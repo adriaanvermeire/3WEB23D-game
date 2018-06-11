7420e12bcdf3b5601cbe63c9dabc7689e79ed152
JeroenGoyvaerts <jeroen.goyvaerts@student.kdg.be>
2018-06-11 19:21:28 +0200

    small change

--- 3WEB23D_unity-project/3WEB23D/Assets/character/scripts/CameraMovement.cs
+++ 3WEB23D_unity-project/3WEB23D/Assets/character/scripts/CameraMovement.cs
@@ -8,6 +8,7 @@ public class CameraMovement : MonoBehaviour {
     public float rotationSpeed = 3.5f;
     public float startRotation = 0;
     private float Y;
+    private float pointer_x = Input.GetAxis("Mouse X");
 
     // Use this for initialization
     void Start () {
@@ -19,6 +20,13 @@ public class CameraMovement : MonoBehaviour {
         if (Input.GetMouseButton(0))
         {
             transform.Rotate(new Vector3(0, -Input.GetAxis("Mouse X") * rotationSpeed, 0));
+            Y = transform.rotation.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(0, Y, 0);
+        }
+        if(Input.touchCount > 0)
+        {
+            pointer_x = Input.touches[0].deltaPosition.x;
+            transform.Rotate(new Vector3(0, -pointer_x * rotationSpeed, 0));
             Y = transform.rotation.eulerAngles.y;
             transform.rotation = Quaternion.Euler(0, Y, 0);
         }
